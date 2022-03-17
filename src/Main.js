eee686df6358e33f144bd7646c706219fab6acc9
Gabriel Cortés Garcia <tetradog@gmail.com>
2022-03-17 19:29:44 +0100

    desarrollando menu
    desarrollando color menuItem

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,27 @@
 $(function () {
 
-
+    var DicMenu = {};
+    var DicMenuMethodUpdate = {};
+    var DicMenuItemColors = {};
     $('#container').show();
     $('#updateBanner').click(function () {
         $(this).hide();
     });
+
+    /*
+        
+        <div id="presentacionAdelaida" class="row"></div>
+        <div id="origenReiki" class="row"></div>
+        <div id="introduccionReiki" class="row"></div>
+        <div id="cursos" class="row"></div>
+        <div id="terapias" class="row"></div>
+        <div id="circulos" class="row"></div>
+        <div id="siemprEnContacto" class="row"></div>
+        <div id="mensaje" class="row"></div>
+        <div id="contacto" class="row"></div>
+        <div id="links" class="row"></div>
+     
+     */
 
     //pongo la información de la web
     Data.GetFrasesInspiradoras().then(frases => {
@@ -37,6 +54,72 @@ $(function () {
 
 
 
+    function loadMenuEInicializa(idParent, itemColorEnable, itemColorDisabled, metodoUpdateItem, arrayDataItems) {
+
+        const ISON = 'isOn';
+        const DISABLED = 1;
+        const ENABLED = 0;
+        
+        var menuItem;
+        var idMenu;
+        var idMenuItemPrefix;
+        var idMenuItem;
+        var menu;
+
+        if (arrayDataItems.length > 1) {
+
+            idMenu = 'menu_' + idParent;
+            idMenuItemPrefix = 'item_' + idParent + '_';
+            menu = '<div id="' + idMenu + '"></div>';
+
+            $('#' + idParent).append(menu);
+            $('#' + idMenu).load('src/Menu.html');
+            $('#aux_menu').attr('id', idMenu);
+            DicMenuData[idMenuItemPrefix] = arrayDataItems;
+            DicMenuMethodUpdate[idMenuItemPrefix] = metodoUpdateItem;
+            DicMenuItemColors[idMenuItemPrefix] = [itemColorEnable, itemColorDisabled];
+            for (var i = 0; i < arrayDataItems.length; i++) {
+                idMenuItem = idMenuItemPrefix + i;
+                menuItem = '<div id="' + idMenuItem + '"></div>';
+                $('#' + idMenu).append(menuItem);
+                $('#' + idMenuItem).load('src/MenuItem.html');
+                $('#aux_menuItem').attr('id', idMenuItem);
+                $('#' + idMenuItem).attr('prefix', idMenuItemPrefix);
+                $('#' + idMenuItem).attr('pos', i);
+                $('#' + idMenuItem).click(function () {
+
+                    var prefix = $(this).attr('prefix');
+                    var pos = parseInt($(this).attr('pos'));
+
+                    if (!$(this).hasClass(ISON)) {
+
+
+                        for (var j = 0, jF = DicMenu[prefix].length; j < jF; j++) {
+                            if (j != pos) {
+                                $('#' + prefix + j).removeClass(ISON);
+                                $('#' + prefix + j).css('background-color', DicMenuItemColors[prefix][DISABLED]);
+                            }
+                        }
+
+                        $(this).addClass(ISON);
+                        $(this).css('background-color', DicMenuItemColors[prefix][ENABLED]);
+
+                        DicMenuMethodUpdate[idMenuItemPrefix](DicMenu[prefix][pos]);
+                    }
+
+
+                });
+            }
+            //pongo el primer item
+            $('#' + idMenuItemPrefix + '0').click();
+
+        } else if (arrayDataItems.length == 1) {
+            //si no hay más elementos no añado el menu y solo pongo el primer item
+            metodoUpdateItem(arrayDataItems[0]);
+        }
+
+
+    }
 
 
 
@@ -47,5 +130,5 @@ $(function () {
 
 
 
-    
+
 });
